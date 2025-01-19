888583020580396d70e49d2542c2e0a423565589
Luishiño <pericena12@gmail.com>
2025-01-19 17:04:13 -0400

    refactor:Se actualizo el archivo app para correr en web

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 # Ruta para la página principal
 @app.route('/')
 def home():
-    return render_template('v3/index.html')
+    return render_template('index.html')
 
 # Ejecutar la aplicación
 if __name__ == '__main__':
